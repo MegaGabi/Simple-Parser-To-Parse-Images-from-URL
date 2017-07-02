1fd2bcb8bc63ea6fe09b55138a81844d65c1f438
Artem <megagabi@ukr.net>
2017-07-02 22:16:27 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Parset
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Parse = new System.Windows.Forms.Button();
+            this.tb_URL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Parse
+            // 
+            this.btn_Parse.Location = new System.Drawing.Point(12, 64);
+            this.btn_Parse.Name = "btn_Parse";
+            this.btn_Parse.Size = new System.Drawing.Size(75, 23);
+            this.btn_Parse.TabIndex = 0;
+            this.btn_Parse.Text = "Parse";
+            this.btn_Parse.UseVisualStyleBackColor = true;
+            this.btn_Parse.Click += new System.EventHandler(this.btn_Parse_Click);
+            // 
+            // tb_URL
+            // 
+            this.tb_URL.Location = new System.Drawing.Point(68, 12);
+            this.tb_URL.Name = "tb_URL";
+            this.tb_URL.Size = new System.Drawing.Size(231, 20);
+            this.tb_URL.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "URL: ";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(68, 38);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "To Parse:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 98);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_URL);
+            this.Controls.Add(this.btn_Parse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Parse;
+        private System.Windows.Forms.TextBox tb_URL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Parset
+{
+    public partial class Form1 : Form
+    {
+        string path;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Parse_Click(object sender, EventArgs er)
+        {
+            var fbd = new FolderBrowserDialog();
+
+            if(path!=null)
+                fbd.SelectedPath = path;
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                path = fbd.SelectedPath;
+
+                GetWebpage(tb_URL.Text);
+            }
+        }
+
+        private void browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            var browser = (WebBrowser)sender;
+            var client = new WebClient();
+            int ind = 0;
+
+            string htmlCode = browser.DocumentText;
+
+            string pattern = "< img.+? src =[\"'](.+?)[\"'].+?>";
+
+            foreach (Match m in Regex.Matches(htmlCode, "<img.+?src=[\"'](.+?)[\"'].+?>", RegexOptions.IgnoreCase | RegexOptions.Multiline))
+            {
+                if (ind >= numericUpDown1.Value)
+                {
+                    break;
+                }
+
+                string src = m.Groups[1].Value;
+
+                if (!Uri.IsWellFormedUriString(src, UriKind.Absolute))
+                {
+                    src = string.Concat(browser.Document.Url.AbsoluteUri, "/", src);
+                }
+
+                try
+                {
+                    var filename = Path.Combine(path, ind + ".png");
+
+                    var req = WebRequest.Create(src);
+                    var imagestream = req.GetResponse().GetResponseStream();
+                    var bmp = Image.FromStream(imagestream);
+                    bmp.Save(filename);
+
+                    imagestream.Dispose();
+                    bmp.Dispose();
+
+                    ind++;
+                }
+                catch { }
+            }
+        }
+
+        private void GetWebpage(string url)
+        {
+            WebBrowser browser = new WebBrowser();
+            browser.Navigate(url);
+            browser.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(browser_DocumentCompleted);
+        }
+        /*
+        void browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            var browser = (WebBrowser)sender;
+            var client = new WebClient();
+            int ind = 0;
+
+            HtmlElementCollection ecol = browser.Document.GetElementsByTagName("img");
+
+            foreach (HtmlElement img in ecol)
+            {
+                if(ind >= numericUpDown1.Value)
+                {
+                    break;
+                }
+
+                var src = img.GetAttribute("src");
+
+                if (!Uri.IsWellFormedUriString(src, UriKind.Absolute))
+                {
+                    src = string.Concat(browser.Document.Url.AbsoluteUri, "/", src);
+                }
+
+                var req = WebRequest.Create(src);
+
+                //Append any path to filename as needed
+                var filename = Path.Combine(path, ind + ".png");
+
+                var imagestream = req.GetResponse().GetResponseStream();
+                var bmp = Image.FromStream(imagestream);
+                bmp.Save(filename);
+
+                imagestream.Dispose();
+                bmp.Dispose();
+
+                //var image = img as HtmlElement;
+                //var src = image.GetAttribute("src");
+
+                //if (!Uri.IsWellFormedUriString(src, UriKind.Absolute))
+                //{
+                //    src = string.Concat(browser.Document.Url.AbsoluteUri, "/", src);
+                //}
+
+                ////Append any path to filename as needed
+                //var filename = Path.Combine(path, ind + ".png");
+
+                //try
+                //{
+                //    File.WriteAllBytes(filename, client.DownloadData(src));
+
+                //    ind++;
+                //}
+                //catch {}
+            }
+        }*/
+
+    }
+}
